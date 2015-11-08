a80baf1a1313eb0cc064273042915813bfe68deb
Adam Coomes <adamcoomes@Adams-MacBook.home>
2015-11-08 16:35:42 -0600

    Beta 1.32
    Removed an admin function

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -627,11 +627,11 @@ module.exports = {
 
 	//REMOVE THIS BEFORE PRODUCTION
 
-	list: function(req, res) {
-		Link.find().populate('info').populate('tags').exec(function(err, links) {
-			res.send(links);
-		});
-	}
+	// list: function(req, res) {
+	// 	Link.find().populate('info').populate('tags').exec(function(err, links) {
+	// 		res.send(links);
+	// 	});
+	// }
 
 
 	// destroyAll: function(req, res) {
